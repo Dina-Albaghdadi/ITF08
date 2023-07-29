84677f5ba608c8ca73bbb0520fb383b668e74aa4
Dina-Albaghdadi <dinalbaghdadi@gmail.com>
2023-07-29 11:59:17 +0300

    fourth commit

---
+++ Task2.py
@@ -0,0 +1,25 @@
+# print("Hello world")
+#
+# x=20
+# if x==20:
+#       print ("yes")
+#       print("continue")
+# Name="Ali"
+# name='ali'
+# print(Name)
+# print(name)
+# print(type(Name))
+# print(type(name))
+# age= input("Enter your ade:")
+# print as
+Name=input("Enter your name:")
+print(Name)
+age=input("Enter your age:")
+print(age)
+BD=input("Enter your Birthdate:")
+print(BD)
+
+
+
+
+
