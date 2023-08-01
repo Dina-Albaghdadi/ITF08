54ef8d8f69e03347542b786a8e19f65b4a6f54e9
Dina-Albaghdadi <dinalbaghdadi@gmail.com>
2023-08-01 12:41:09 +0300

    first commit

--- Task3.py
+++ Task3.py
@@ -12,12 +12,17 @@
 # print(type(name))
 # age= input("Enter your ade:")
 # print as
-Name=input("Enter your name:")
-print(Name)
-age=input("Enter your age:")
-print(age)
-BD=input("Enter your Birthdate:")
-print(BD)
+# Name=input("Enter your name:")
+# print(Name)
+# age=input("Enter your age:")
+# print(age)
+# BD=input("Enter your Birthdate:")
+# print(BD)
+# x=25.0
+# x=str(x)
+# print(type(x))
+
+
 
 
 
