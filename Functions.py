8f11564716c69f406caac0c7476f33975daa00a4
Dina-Albaghdadi <dinalbaghdadi@gmail.com>
2023-08-02 16:46:43 +0300

    second commit

---
+++ Functions.py
@@ -0,0 +1,24 @@
+num1 = int(input("Enter the first number:"))
+num2 = int(input("Enter the second number:"))
+def add_function(num1,num2):
+    return num1 + num2
+def sub_function(num1,num2):
+    return num1 - num2
+def multi_function(num1,num2):
+    return num1*num2
+def division_function(num1,num2):
+    return num1/num2
+total= add_function(num1,num2)
+print(total)
+subtraction= sub_function(num1,num2)
+print(subtraction)
+multiplication= multi_function(num1,num2)
+print(multiplication)
+division= division_function(num1,num2)
+print(division)
+
+
+
+
+
+
