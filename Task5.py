1910a70377b540e3a065735a52bec86af9136bd5
Dina-Albaghdadi <dinalbaghdadi@gmail.com>
2023-08-05 11:36:51 +0300

    fourth commit

--- Task5.py
+++ Task5.py
@@ -1,11 +1,11 @@
 def circle():
-    r = int(input("Enter the raduis:"))
-    area = r * r * 3.14
-    circle_area=area
-    perimeter =2*3.14*r
-    print(circle_area)
-    print(perimeter)
-circle()
+        r = int(input("Enter the raduis:"))
+        area = r * r * 3.14
+        circle_area=area
+        perimeter =2*3.14*r
+        print(circle_area)
+        print(perimeter)
+
 
 def Triangle():
         base = int(input("Enter the base:"))
@@ -17,22 +17,23 @@ def Triangle():
         print(Triangle_area)
         print(perimeter)
 
-Triangle()
 def rectangular():
-    length = int(input("Enter the length:"))
-    width = int(input("Enter the width:"))
-    area=length*width
-    rectangular_area=area
-
-    print(rectangular_area)
-rectangular()
+        length = int(input("Enter the length:"))
+        width = int(input("Enter the width:"))
+        area=length*width
+        rectangular_area=area
+        print(rectangular_area)
 
 
-    if area >= 10:
-        print("big")
-    elif area >0 and area <10 :
-        print("small")
-    else:
-         print("invalid input")
+
+        if area >= 10:
+            print("big")
+        elif area<10 and area>0:
+            print("small")
+        else:
+            print("invalid input")
+
 
 circle()
+Triangle()
+rectangular()
