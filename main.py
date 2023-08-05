0818077e9242235e034ccaf771df643b23db19b4
Dina-Albaghdadi <dinalbaghdadi@gmail.com>
2023-08-05 07:09:19 +0300

    Third commit

--- main.py
+++ main.py
@@ -17,3 +17,47 @@
 # print("Dina")
 # print("Dina")
 # input("Enter your name")
+# num =str(2.5)
+# print(type(num))
+#
+# num2="2.5"
+# print(type(num2))
+# num3=2.5
+# num3=str(num3)
+# print(type(num3))
+#text="GazaskyGeeks"
+# my_text=text
+# print(my_text)
+# my_text=text[:]
+# print(text)
+# print(text.title())
+# # text=text.replace("a","@")
+# print(text)
+# text = text.replace(text[1:-1]("*******")
+#الفاصلة مهمة جدا و كذلك مش من الضروري انه عدد النجوم يساوي عدد الاحرف اللي بدي اخفيها
+# text = text.replace(text[1:-1],"******")
+#عشان احنا بدنا نخفي من ثاني حرف للحرف القبل الاخير لذلك نبدأ من 1 الى اخر رمز بدي ياه يختفي بعمل للاندكس تاعه+1
+#يعني بمثال هلأ اخر رمز بدي اخفيه هو ال كي الاندكس اللي لاله هو -2 اذن -2+1 يساوي -1
+#print(text)
+# numbers = 2.5+2
+# print(numbers)
+# x=10 #int
+# y=10.5 #float
+# print(x is y) #int != float
+# x=5
+# y=5.0
+# print(x == y)
+# name = 'Dina'
+# print('D' not in name)
+# print(name.index("D"))
+# text="Dina Albaghdadi"
+# text = text.replace(text[1:-1],("****"))   #text[1:-1]
+# print(text)
+# print(text[0:4])
+# def calculate_age ():
+#     year_of_birth = input("Enter.....")
+#     age = 2023 - int(year_of_birth)
+#     print(age)
+# calculate_age ()
+input("Enter your name")
+
