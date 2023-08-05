cc6e19b9eb36cb8a70d46108147927a6a09924b1
Dina-Albaghdadi <dinalbaghdadi@gmail.com>
2023-08-05 07:12:15 +0300

    fourth commit

---
+++ Square.py
@@ -0,0 +1,8 @@
+def Square ():
+    side = int(input("Enter the side of the square:"))
+    area_of_the_square = side*side
+    perimeter_of_the_square = side*4
+    print(area_of_the_square)
+    print(perimeter_of_the_square)
+Square()
+
---
+++ Triangle.py
@@ -0,0 +1,13 @@
+def Triangle ():
+    base = int(input("Enter the base:"))
+    height = int(input("Enter the height:"))
+    side = int(input("Enter the side of the triangle:"))
+    area = 0.5*base*height
+    perimeter = side*3 #محيط مثلث متساوي الأضلاع
+    print(area)
+    print(perimeter)
+Triangle ()
+
+
+
+
