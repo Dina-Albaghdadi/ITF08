a7c4b5c963eb7dac21e6ad7177fa4622f0e12d08
Dina-Albaghdadi <dinalbaghdadi@gmail.com>
2023-08-16 15:54:45 +0300

    first commit

--- marks.py
+++ marks.py
@@ -1,18 +1,11 @@
-counter = 0
-while True:
-    students = []
-    number_of_students = int(input("Enter the number of the students:"))
-    for i in range(0,number_of_students):
-        mark = float(input(f"Enter the number of the marks for student {i+1}:"))
-        students.append(mark)
-    my_marks = []
-    number_of_marks = int(input("Enter the total number of the marks for student:"))
-    for i in range(0,number_of_marks):
-        mark = float(input(f"Enter mark {i+1}:"))
-        my_marks.append(mark)
-    average = sum(my_marks) / len(my_marks)
-    average = round(average,1)
-    print("The average is:", average)
-    print('Max mark is:', max(my_marks))
-    print('Min mark is:', min(my_marks))
-counter +=1
+students = int(input("Enter the number of the students:"))
+for i in range (students):
+    marks_number = int(input("Enter total marks number:"))
+    marks = []
+    for j in range(marks_number):
+        mark = float(input(f"Enter student {i+1} mark {j+1}:"))
+        marks.append(mark)
+    average = sum(marks)/ len(marks)
+    print(f"student{i+1} average =", average)
+    print(f"max mark for student {i+1} =", max(marks))
+    print(f"min mark for student {i+1} =", min(marks))
